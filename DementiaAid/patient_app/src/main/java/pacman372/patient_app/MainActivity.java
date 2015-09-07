8a9940c32c1b087145c870e7d0dde79ed6ec9553
Cynthia <qiananger1234@gmail.com>
2015-09-08 01:39:46 +1000

    update "share location" on patient phone

--- DementiaAid/patient_app/src/main/java/pacman372/patient_app/MainActivity.java
+++ DementiaAid/patient_app/src/main/java/pacman372/patient_app/MainActivity.java
@@ -1,13 +1,30 @@
 package pacman372.patient_app;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import org.json.JSONException;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.logging.LogRecord;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,7 +35,123 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButtonHandler();
+        Button buttonShare=(Button)findViewById(R.id.button);
+        buttonShare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                try {
+                    shareLocation();
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
     }
+
+
+
+
+    private class DownloadWebpageTask extends AsyncTask<String, Void, String> {
+
+
+        @Override
+        protected String doInBackground(String... urls) {
+
+            return downloadUrl(urls[0]);
+
+        }
+
+        @Override
+        public void onPostExecute(String result){
+            TextView text=(TextView)findViewById(R.id.textView);
+            text.setText("Share Successfully");
+        }
+
+
+        public String downloadUrl(String targetURL){
+            URL url;
+            String urlParameters="coordinates_x=27&coordinates_y=153&id_patient=9999&id_carer=9999";
+            HttpURLConnection connection = null;
+            try {
+                //Create connection
+                url = new URL(targetURL);
+                connection = (HttpURLConnection)url.openConnection();
+                connection.setRequestMethod("POST");
+                connection.setRequestProperty("Content-Type",
+                        "application/x-www-form-urlencoded");
+
+                connection.setRequestProperty("Content-Length", "" +
+                        Integer.toString(urlParameters.getBytes().length));
+
+
+                connection.setUseCaches (false);
+                connection.setDoInput(true);
+                connection.setDoOutput(true);
+
+                //Send request
+                DataOutputStream wr = new DataOutputStream (
+                        connection.getOutputStream ());
+                wr.writeBytes (urlParameters);
+                wr.flush ();
+                wr.close ();
+
+                //Get Response
+                InputStream is = connection.getInputStream();
+                BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+                String line;
+                StringBuffer response = new StringBuffer();
+                while((line = rd.readLine()) != null) {
+                    response.append(line);
+                    response.append('\r');
+                }
+                rd.close();
+                return response.toString();
+
+            } catch (Exception e) {
+
+                e.printStackTrace();
+                return null;
+
+            } finally {
+
+                if(connection != null) {
+                    connection.disconnect();
+                }
+            }
+        }
+
+
+    }
+        // onPostExecute displays the results of the AsyncTask.
+
+
+
+
+
+
+
+
+    public void shareLocation() throws UnsupportedEncodingException, JSONException, InterruptedException {
+        String locationUrl="http://pacmandementiaaid.azurewebsites.net/api/Location";
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected()) {
+            new DownloadWebpageTask().execute(locationUrl);
+        }else {
+            LogRecord alertDialog=null;
+            alertDialog.setMessage("No network connection available.");
+        }
+    }
+
+
+
 
     public void ButtonHandler()
     {
@@ -31,9 +164,6 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
-
-
 
 
     }
