0be14946c671edfed305a3e84f3ae0704e9c3030
CHAUNLI <lichuan@LiChuantekiMacBook-Pro.local>
2015-10-13 15:07:50 +1000

    patient client

---
+++ pacmanAndroidNew/app/src/main/java/com/example/pacmanandroidnew/LoginDetail.java
@@ -0,0 +1,17 @@
+package com.example.pacmanandroidnew;
+
+/**
+ * Created by lichuan on 10/10/2015.
+ */
+public class LoginDetail {
+
+
+    @com.google.gson.annotations.SerializedName("id")
+    private String id;
+
+    @com.google.gson.annotations.SerializedName("username")
+    String username;
+    @com.google.gson.annotations.SerializedName("password")
+    String password;
+
+}
