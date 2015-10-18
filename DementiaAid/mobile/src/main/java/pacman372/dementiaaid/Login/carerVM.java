5278e4bfda58d4493fda1080e6f5fe54ad2317eb
GeoffreyByrne <giririsss@giririsss.com>
2015-10-18 19:16:51 +1000

    Updated Login to have patient id set in shard prefs

--- DementiaAid/mobile/src/main/java/pacman372/dementiaaid/Login/carerVM.java
+++ DementiaAid/mobile/src/main/java/pacman372/dementiaaid/Login/carerVM.java
@@ -39,21 +39,24 @@ public class carerVM {
     public final String ERROR_KEY = "carerVM: ";
     private AlertDialog.Builder alertDialog;
     private int IDCarer=-1;
+    private int IDPatient;
     public String uName = "";
     public String uPass = "";
     public String returnedCarerString ="";
     public boolean loggedin = false;
+    public boolean pdetails = false;
     private Activity caller;
+
 
     public boolean login(Activity caller, String uName, String uPass) {
         this.caller = caller;
         try {
-            new DownloadWebpageTask().execute("http://pacmandementiaaid.azurewebsites.net/api/Login/" + uName + "/" + uPass).get();
+            new DownloadLoginTask().execute("http://pacmandementiaaid.azurewebsites.net/api/Login/" + uName + "/" + uPass).get();
         } catch(Exception e) { }
 
         if (loggedin) {
             loggedIn();
-            new DownloadWebpageTask().execute("http://pacmandementiaaid.azurewebsites.net/api/Login/" + uName + "/" + uPass);
+            new DownloadPatientTask().execute("http://pacmandementiaaid.azurewebsites.net/api/CarerPatient?$filter=id_carer eq "+IDCarer);
             return true;
         } else {
             return false;
@@ -61,20 +64,24 @@ public class carerVM {
     }
 
     public void loggedIn() {
-        try {
-            JSONObject jsonObject = new JSONObject(returnedCarerString);
-            carer.setID(jsonObject.getInt("ID"));
-            SharedPreferences userDetails = caller.getSharedPreferences(caller.getString(R.string.sharedPreferences),0);
-            SharedPreferences.Editor editor = userDetails.edit();
-            editor.putString("userID",String.valueOf(carer.getID()));
-            editor.commit();
-        } catch (JSONException e) {
 
-        }
-
+        //JSONObject jsonObject = new JSONObject(returnedCarerString);
+        //carer.setID(jsonObject.getInt("ID"));
+        SharedPreferences userDetails = caller.getSharedPreferences(caller.getString(R.string.sharedPreferences),0);
+        SharedPreferences.Editor editor = userDetails.edit();
+        //editor.putString("userID",String.valueOf(carer.getID()));
+        editor.putString("userID",String.valueOf(IDCarer));
+        editor.commit();
     }
 
-    private class DownloadWebpageTask extends AsyncTask<String, Void, String> {
+    public void patientID() {
+        SharedPreferences userDetails = caller.getSharedPreferences(caller.getString(R.string.sharedPreferences),0);
+        SharedPreferences.Editor editor = userDetails.edit();
+        editor.putString("patientID",String.valueOf(IDPatient));
+        editor.commit();
+    }
+
+    private class DownloadLoginTask extends AsyncTask<String, Void, String> {
 
         @Override
         protected String doInBackground(String... urls) {
@@ -128,6 +135,78 @@ public class carerVM {
                     JSONObject jsonObject = new JSONObject(contentAsString);
                     IDCarer = jsonObject.getInt("ID");
                     loggedin = true;
+                } else {
+                    Log.d(ERROR_KEY, "Failed Web Call");
+                }
+                return contentAsString;
+            } catch (JSONException e) {
+                Log.d(ERROR_KEY, "Json Error", e);
+                return "Error";
+            } finally {
+                if (is != null) {
+                    is.close();
+                }
+            }
+
+        }
+
+        public String readIt(InputStream stream, int len) throws IOException, UnsupportedEncodingException {
+            Reader reader = null;
+            reader = new InputStreamReader(stream, "UTF-8");
+            char[] buffer = new char[len];
+            reader.read(buffer);
+            return new String(buffer);
+        }
+
+    }
+    private class DownloadPatientTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... urls) {
+
+            // params comes from the execute() call: params[0] is the url.
+            try {
+                return downloadUrl(urls[0], 1);
+            } catch (IOException e) {
+                return "Unable to retrieve web page. URL may be invalid.";
+            }
+        }
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        public void onPostExecute(String result) {
+            patientID();
+        }
+
+
+
+        private String downloadUrl(String myurl, int n) throws IOException {
+            InputStream is = null;
+            // Only display the first 500 characters of the retrieved
+            // web page content.
+            Location location = new Location();
+            int len = 500;
+
+            try {
+                URL url = new URL(myurl);
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestProperty("Content-Type", "application/json");
+                conn.setReadTimeout(10000 /* milliseconds */);
+                conn.setConnectTimeout(15000 /* milliseconds */);
+                conn.setDoOutput(false);
+                conn.setDoInput(true);
+                conn.setRequestMethod("GET");
+
+                // Starts the query
+                conn.connect();
+                int response = conn.getResponseCode();
+                is = conn.getInputStream();
+                String contentAsString = readIt(is, len);
+                if (response == 200)
+                {
+                    // Convert the InputStream into a string
+                    JSONObject jsonObject = new JSONObject(contentAsString);
+                    IDPatient = jsonObject.getInt("id_patient");
+                    pdetails = true;
                 } else {
                     Log.d(ERROR_KEY, "Failed Web Call");
                 }
